c31a6be19cd703bfe0e8c7c44cf45c1e4ccf1189
Luc <luc.aggett@gmail.com>
2022-06-19 19:38:40 +0200

    Initial commit

---
+++ oraculum.py
@@ -0,0 +1,22 @@
+"""
+Oraculum is the softwware part of the oraculum machine.
+
+See it at sub divo festival!
+"""
+
+import translators
+
+def translate(text):
+    """
+    Translate quotes.
+    """
+    pass
+
+
+
+
+def get_random_quote():
+    """
+    Get a random quote.
+    """
+    pass
