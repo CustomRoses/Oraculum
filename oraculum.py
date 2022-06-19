593296dce2734cc7299259ceb8a8837289f74b73
Luc <luc.aggett@gmail.com>
2022-06-19 22:26:07 +0200

    Add basic functionality for the oraculum class

--- oraculum.py
+++ oraculum.py
@@ -4,7 +4,7 @@ Oraculum is the softwware part of the oraculum machine.
 See it at sub divo festival!
 """
 
-import translators as ts
+from argostranslate import translate
 import random
 
 
@@ -17,25 +17,65 @@ def fix_file(file_name):
         lines = f.readlines()
     if file_name == "quotes_lienying.txt":
         for line in lines:
-            line = " ".join(line.split()[1:])
-            text += line
+            sp = line.strip().split(" ")
+            cutoff = 1 if ")" in sp[0] else 2
+            print(sp[cutoff:], cutoff)
+            text += " ".join(sp[cutoff:]) + "\n"
         print(text)
         with open(f"{file_name}_fixed", 'w') as f:
             f.write(text)
 
 
-
-def translate(text):
+class Oraculum:
     """
-    Translate quotes.
+    Oraculum is the softwware part of the oraculum machine.
     """
-    return ts.deepl(text)
+    def __init__(self):
+        self.filepath_DE = "weisheiten_DE"
+        self.filepath_LAT = "weisheiten_LAT"
+        with open(self.filepath_DE, 'r', encoding="utf-8") as f:
+            lines_DE = f.readlines()
+        with open(self.filepath_LAT, 'r', encoding="utf-8") as f:
+            lines_LAT = f.readlines()
+
+        self.quotes = {}
+        for de, lat in zip(lines_DE, lines_LAT):
+            self.quotes[de.strip()] = lat.strip()
+
+
+    def get_random_quote(self):
+        """
+        Get a random quote.
+        """
+        return random.choice(list(self.quotes.items()))
+
+    def __len__(self):
+        return len(self.quotes)
+
+    def __getitem__(self, key):
+        return self.quotes[key]
+
+    def __setitem__(self, key, value):
+        self.quotes[key] = value
+
+    def __delitem__(self, key):
+        del self.quotes[key]
+
+    def __contains__(self, key):
+        return key in self.quotes
+
+    def __iter__(self):
+        return iter(self.quotes)
+
+    def __reversed__(self):
+        return reversed(self.quotes)
+
+    def __str__(self):
+        return str(self.quotes)
+
+    def __repr__(self):
+        return repr(self.quotes)
 
 
 
 
-def get_random_quote():
-    """
-    Get a random quote.
-    """
-    return random.choice(open("quotes_lienying.txt").readlines())
