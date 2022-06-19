3fb26460eba69bcf43dcb3c734cd08349d3c738b
Luc <luc.aggett@gmail.com>
2022-06-19 20:53:18 +0200

    Initial commit

--- oraculum.py
+++ oraculum.py
@@ -4,13 +4,32 @@ Oraculum is the softwware part of the oraculum machine.
 See it at sub divo festival!
 """
 
-import translators
+import translators as ts
+import random
+
+
+def fix_file(file_name):
+    """
+    Fix a file.
+    """
+    text = ""
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    if file_name == "quotes_lienying.txt":
+        for line in lines:
+            line = " ".join(line.split()[1:])
+            text += line
+        print(text)
+        with open(f"{file_name}_fixed", 'w') as f:
+            f.write(text)
+
+
 
 def translate(text):
     """
     Translate quotes.
     """
-    pass
+    return ts.deepl(text)
 
 
 
@@ -19,4 +38,4 @@ def get_random_quote():
     """
     Get a random quote.
     """
-    pass
+    return random.choice(open("quotes_lienying.txt").readlines())
